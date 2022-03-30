88a5f4dcec91a0d8b135294f20019f0988e6aa42
Michael <massolmichael@gmail.com>
2022-03-30 12:50:49 +0200

    Made a tree structure for the code

---
+++ code/EN/techit_home_page.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Style.css">
+  <title>Tech It - Home Page</title>
+</head>
+<body>
+  <header>
+	<div>
+  	<img class="logo" src="logo_tech_it.png" alt="logo"/>
+	<p style="text-align:center;">Slogan</p>
+	</div>
+    <nav>
+	 <input placeholder="What do you want ?">
+      <ul class="nav_links">
+	  <li><a href="#">Get equipment</a></li>
+	  <li><a href="#">Donate</a></li>
+	  <li><a href="#">About</a></li>
+      </ul>
+    </nav>
+	<a class="contact" href="#"><button>Contact us</button></a>
+  </header>
+  <h1>Presentation</h1>
+  <p>Lorem ipsum dolor sit amet. Vel reiciendis dolorem et laboriosam voluptatem sed accusamus dolorum sed laborum amet. Et neque iste id omnis quia 
+  eos vitae corporis ea soluta enim aut voluptas nemo sit architecto maxime. Qui galisum quisquam quo consequatur delectus sed enim accusantium. A alias
+  consequuntur est voluptatum voluptate qui minus suscipit est voluptate nihil est animi velit et totam nobis. Est totam mollitia qui vero omnis est 
+  perspiciatis alias qui dolor pariatur eum corporis obcaecati et laudantium eaque est molestiae aspernatur. Sed dolores magni non atque vitae eum maiores error. 
+  Et eius aspernatur qui quia voluptas eos numquam quaerat?Eum commodi voluptatum vel maxime galisum vel omnis mollitia ex repellendus quia. Et autem vero aut 
+  voluptatibus molestias rem dolore necessitatibus! Aut repudiandae labore ut blanditiis expedita vel obcaecati consectetur et quas natus et ipsam ipsum.</p>
+	<ul class="item">
+		<img src="PC.jpg"/>
+		<li>PC</li>
+	</ul>
+<div class="block">
+  </div>
+</body>
+</html>
